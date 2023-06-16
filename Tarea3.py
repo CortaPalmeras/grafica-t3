61210bc4da015fe2f5f3ea7930990895262e12dc
CortaPalmeras <lucasllort@gmail.com>
2023-06-16 04:50:47 -0400

    neblina añadida :D

--- Tarea3.py
+++ Tarea3.py
@@ -7,6 +7,19 @@ import libs.easy_shaders as sh
 import libs.gpu_shape as gp
 from libs.basic_shapes import Shape
 
+# OPENGL SETUP
+win = pyglet.window.Window()
+win.set_exclusive_mouse(True)
+win.set_mouse_visible(False)
+program = sh.FogViewProjectionShaderProgram()
+glUseProgram(program.shaderProgram)
+glEnable(GL_DEPTH_TEST)
+glEnable(GL_CULL_FACE)
+glEnable(GL_BLEND)
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+glClearColor(0.5, 0.5, 0.8, 1.0)
+
+# CARGAR NAVE
 with open("assets\ship_cords.txt", "r") as file:
     ship_vert = np.array(file.read().replace("\n"," ").split(" "))
 
@@ -21,33 +34,51 @@ ship_ind = (0,1,4, 1,2,4, 2,3,4, 3,0,4, 3,2,5, 2,1,5, 1,0,5, 0,3,5,
             14,15,16, 17,19,18, 16,15,18, 14,18,15,
             22,21,20, 24,25,23, 24,21,22, 20,21,24)
 
-ind_lines = (0,1, 1,2, 2,3, 3,0, 4,0, 4,1, 4,2, 4,3, 5,0,5,1, 5,2, 5,3,
+lines_ind = (0,1, 1,2, 2,3, 3,0, 4,0, 4,1, 4,2, 4,3, 5,0,5,1, 5,2, 5,3,
             6,7, 7,8, 8,6, 6,9, 9,7, 9,8,
             10,11, 11,12, 12,10, 10,13, 13,11, 13,12,
             14,15, 15,16, 16,17, 17,18, 18,19, 15,18,
             24,21, 25,24, 24,23, 23,22, 22,21, 21,20) 
 
-#crear suelo
-
-
-win = pyglet.window.Window()
-win.set_exclusive_mouse(True)
-win.set_mouse_visible(False)
-program = sh.SimpleModelViewProjectionShaderProgram()
-glUseProgram(program.shaderProgram)
 ship = gp.createGPUShape(program, Shape(ship_vert, ship_ind))
-lines =  gp.createGPUShape(program, Shape(dark_vert, ind_lines))
+lines =  gp.createGPUShape(program, Shape(dark_vert, lines_ind))
 
-glEnable(GL_DEPTH_TEST)
-glEnable(GL_CULL_FACE)
-glClearColor(0.15, 0.15, 0.17, 1.0)
+# CREAR SUELO
+length = 30
+count = 24
+box_len = 2 * length / count
+floor_vert = ()
+color = True
+for x in range(count):
+    for z in range(count):
+        for u in range(2):
+            for v in range(2):
+                floor_vert += ((x+u)*box_len-length, 0, (z+v)*box_len-length)
+                if color:
+                    floor_vert += (0.43, 0.73, 0.43)
+                else:
+                    floor_vert += (0.3, 0.6, 0.3)
+        color = not color
+    color = not color
 
-view = tr.lookAt(np.array([0,0,2]), np.array([0,0,0]), np.array([0,1,2]))
-glUniformMatrix4fv(glGetUniformLocation(program.shaderProgram, "view"), 1, GL_TRUE, view)
+floor_ind = ()
+for i in range(count**2):
+    j = i*4
+    floor_ind += (j, j+1, j+2, j+1, j+3, j+2)
+
+floor = gp.createGPUShape(program, Shape(floor_vert, floor_ind))
+
+def ad_infinitum(pos):
+    return tr.translate(5*(pos[0]//5), 0, 5*(pos[2]//5))
+
+# SET TRANSFORMS
+view = tr.lookAt(np.array([0,2,3]), np.array([0,2,0]), np.array([0,1,3]))
 ratio = win.aspect_ratio
-projection = tr.frustum(-0.5 * ratio, 0.5 * ratio, -0.5, 0.5, 0.5, 4)
+projection = tr.perspective(60, ratio, 0.5, 100)
+glUniformMatrix4fv(glGetUniformLocation(program.shaderProgram, "view"), 1, GL_TRUE, view)
 glUniformMatrix4fv(glGetUniformLocation(program.shaderProgram, "projection"), 1, GL_TRUE, projection)
-rot_loc = glGetUniformLocation(program.shaderProgram, "model")
+model_loc = glGetUniformLocation(program.shaderProgram, "model")
+shipPos_loc = glGetUniformLocation(program.shaderProgram, "shipPos")
 
 theta = 0.0
 phi = 0.0
@@ -55,17 +86,19 @@ rotate_left = False
 rotate_right = False
 forward = False
 backward = False
-position = np.zeros(3)
+position = np.array([0, 2, 0], dtype=np.float32)
 
 @win.event
 def on_draw():
+    global theta, position
     glClear(GL_COLOR_BUFFER_BIT)
     glClear(GL_DEPTH_BUFFER_BIT)
     glUseProgram(program.shaderProgram)
     program.drawCall(ship, GL_TRIANGLES)
     program.drawCall(lines, GL_LINES)
+    glUniformMatrix4fv(model_loc, 1, GL_TRUE, ad_infinitum(position))
+    program.drawCall(floor, GL_TRIANGLES)
 
-    global theta, position
     if rotate_left:
         theta += 0.05
     if rotate_right:
@@ -79,17 +112,18 @@ def on_draw():
     direction = np.array((-s*c2,s2,-c*c2))
 
     if forward:
-        if position[1]<=0 and phi<0:
+        if position[1]<=1 and phi<0:
             direction[1] = 0
-        position += direction * 0.05   # pasar todo ewsto a una funcion update()
+        position += direction * 0.05   # pasar todo esto a una funcion update()
 
     elif backward:
-        if (position[1]<=0 and phi>0):
+        if position[1]<=1 and phi>0:
             direction[1] = 0
         position -= direction * 0.05
 
-    transform = tr.translate(*position) @ tr.rotationY(theta) @ tr.rotationX(phi)
-    glUniformMatrix4fv(rot_loc, 1, GL_TRUE, transform)
+    model = tr.translate(*position) @ tr.rotationY(theta) @ tr.rotationX(phi)
+    glUniformMatrix4fv(model_loc, 1, GL_TRUE, model)
+    glUniform3f(shipPos_loc, *position)
     
 @win.event
 def on_mouse_motion(x, y, dx, dy):
@@ -129,6 +163,7 @@ def on_key_release(symbol, mods):
 def on_close():
     ship.clear()
     lines.clear()
+    floor.clear()
     glDeleteProgram(program.shaderProgram)
 
 pyglet.app.run()
